ee2869a9f329b2f0311a0a7f3fc707c6b05cc803
Dannynsikak <nsikakdanny11@gmail.com>
2024-08-16 15:41:35 +0100

    added form validations to the create user

--- src/component/AddEmployee.tsx
+++ src/component/AddEmployee.tsx
@@ -11,23 +11,57 @@ const AddEmployee = (props: Props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
   const { onBackBtnClickHnd, onSubmitClickHnd } = props;
 
-  const onFirstNameChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
+  const validate = () => {
+    let isValid = true;
+    const newErrors = { firstName: "", lastName: "", email: "" };
+
+    if (firstName.trim() === "") {
+      newErrors.firstName = "First name is required";
+      isValid = false;
+    }
+
+    if (lastName.trim() === "") {
+      newErrors.lastName = "Last name is required";
+      isValid = false;
+    }
+
+    if (email.trim() === "") {
+      newErrors.lastName = "Email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Email is invalid";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
   };
 
-  const onLastNameChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
-  };
+  // const onFirstNameChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setFirstName(e.target.value);
+  // };
 
-  const onEmailChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
+  // const onLastNameChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setLastName(e.target.value);
+  // };
+
+  // const onEmailChangeHnd = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setEmail(e.target.value);
+  // };
 
   const onSubmitbtnClickHnd = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!validate()) {
+      return;
+    }
     const data: IEmployee = {
       id: new Date().toJSON().toString(),
       firstName: firstName,
@@ -54,9 +88,12 @@ const AddEmployee = (props: Props) => {
             type="text"
             id="firstName"
             value={firstName}
-            onChange={onFirstNameChangeHnd}
+            onChange={(e) => setFirstName(e.target.value)}
             className=" p-[1.5em_0] mt-1 block w-full  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.firstName && (
+            <p className="text-red-500 text-sm mt-1">{errors.firstName}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="lastName" className="block text-gray-700">
@@ -66,9 +103,12 @@ const AddEmployee = (props: Props) => {
             type="text"
             id="lastName"
             value={lastName}
-            onChange={onLastNameChangeHnd}
+            onChange={(e) => setLastName(e.target.value)}
             className="mt-1 block w-full p-[1.5em_0] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.lastName && (
+            <p className="text-red-500 text-sm mt-1">{errors.lastName}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700">
@@ -78,9 +118,12 @@ const AddEmployee = (props: Props) => {
             type="text"
             id="email"
             value={email}
-            onChange={onEmailChangeHnd}
+            onChange={(e) => setEmail(e.target.value)}
             className="mt-1 block w-full p-[1.5em_0] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.email && (
+            <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+          )}
         </div>
         <div className="flex justify-between">
           <input
